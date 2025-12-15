aa618b0f4d4dacff0f16fb41a833af69eac24a15
Lucas Ribeiro da Luz <lucasrluz.23@gmail.com>
2025-12-15 20:16:31 -0300

    feat: pegando dados básicos com a lib time

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <time.h>
 
 int main() {
-    printf("Hello, world!\n");
+    time_t now = time(NULL);
+    struct tm *s_now = localtime(&now);
+
+    printf("Dia da semana atual: %d\n", s_now->tm_wday);
+    printf("Dia atual: %d\n", s_now->tm_mday);
+    printf("Mês atual: %d\n", s_now->tm_mon + 1);
+    printf("Ano atual: %d\n", s_now->tm_year + 1900);
 
     return 0;
 }
